9c2f1f5050ef9e05db39550fc0b887f6468062c1
unknown <1500405@MC8174.uad.ac.uk>
2017-02-21 11:32:45 +0000

    new player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,29 @@ public class Player : MonoBehaviour {
 
 	// variables taken from CharacterController.Move example script
 	// https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
-	public float speed = 6.0F;
-	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
+	public float speed;
+	public float jumpSpeed;
+	public float gravity;
 	private Vector3 moveDirection = Vector3.zero;
 
 	public int Lives = 3; // number of lives the player hs
 
+	public GameObject cameraObject;
 
 	Vector3 start_position; // start position of the player
 
+	Vector3 startScale;		//original scale of player
+	Vector3 crouchScale;	//scale of player when crouched
+	public int crouchCounter = 0;	//lets you crouch jump
 
 	void Start()
 	{
 		// record the start position of the player
 		start_position = transform.position;
+
+		//define the transform
+		startScale = gameObject.transform.lossyScale;
+		crouchScale = new Vector3 (startScale.x, startScale.y / 2, startScale.z);
 	}
 
 	public void Reset()
@@ -28,29 +36,69 @@ public class Player : MonoBehaviour {
 		transform.position = start_position;
 	}
 
-	void Update()
+	void Update () 
 	{
-		// get the character controller attached to the player game object
 		CharacterController controller = GetComponent<CharacterController>();
-
-
-		// check to see if the player is on the ground
+		//run
+		if (Input.GetKey ("z")) {
+			speed = 12.0f;
+			if (Mathf.Abs(Input.GetAxis ("Horizontal")) > 0.5f) 	//you can jump higher when running
+			{
+				jumpSpeed = 24.0f;
+			}
+			else 
+			{
+				jumpSpeed = 20.0f;
+			}
+		} 
+		else 
+		{
+			speed = 6.0f;
+			jumpSpeed = 20.0f;
+		}
+			
 		if (controller.isGrounded) 
 		{
-			// set the movement direction based on user input and the desired speed
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
+			//crouch
+			if (Input.GetKey ("down")) 
+			{
+				gameObject.transform.localScale = crouchScale;
+				crouchCounter++;
+				if (crouchCounter > 60)		//crouch for a bit to charge jump
+				{
+					jumpSpeed = 30.0f;
+				}
+			} 
+			else 
+			{
+				gameObject.transform.localScale = startScale;
+				crouchCounter = 0;
+			}
 
-			// check to see if the player should jump
-			if (Input.GetButton("Jump"))
-				moveDirection.y = jumpSpeed;
+			//jump
+			moveDirection.y = 0;					//on the gound there is no vertical movement
+			if (Input.GetKeyDown("x"))
+			{
+				moveDirection.y = jumpSpeed;					//after jumpin the vertical movement = jumpSpeed
+				gameObject.transform.localScale = startScale; 	//uncrouch in air
+				crouchCounter = 0; 								//reset this when jump
+			}
 		}
 
-		// apply gravity to movement direction
-		moveDirection.y -= gravity * Time.deltaTime;
+		if (gameObject.transform.lossyScale.y >= 0.9) 
+		{	//if he isnt crouching essentially
+			moveDirection.x = Input.GetAxis ("Horizontal");					//take horizontal movement from the x axis
+			moveDirection.x *= speed;										//multiply by the speed
+		} 
+		else 
+		{
+			moveDirection.x = 0;			//stop
+		}
 
-		// make the call to move the character controller
-		controller.Move(moveDirection * Time.deltaTime);
+		moveDirection.y -= gravity * Time.deltaTime;		//move down due to gravity
+
+		controller.Move (moveDirection * Time.deltaTime);	//apply the movement
+
+		cameraObject.transform.position = new Vector3 (gameObject.transform.position.x, 3.5f, -13.5f);
 	}
 }
